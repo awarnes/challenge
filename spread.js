348ba5166c3826e86bed640bde02d6d4557f0a70
awarnes <alex.warnes@moovel.com>
2023-01-09 17:40:01 -0800

    First pass on spread.js

--- spread.js
+++ spread.js
@@ -2,3 +2,25 @@
  * Question:
  * What does the spread operator do in JS?
  */
+
+/**
+ * Answer:
+ * The spread operator is used to explode or convert an iterable object into its individual
+ * component pieces.
+ */
+
+const expand = array => console.log(...array);
+
+// Returns 1, 2, 3, 4 as indiviual values
+expand([1, 2, 3, 4]);
+
+// This could be used to convert a string into an array of its characters
+// -> ['H', 'e', 'l', 'l','o', ',', ' ', 'w','o', 'r', 'l', 'd','!']
+console.log([...'Hello, world!']);
+
+// Often this is used to expand an array into the arguments of a function
+const addThenMultiply = (add1, add2, multiply) => (add1 + add2) * multiply;
+
+const inputs = [1, 2, 3];
+// -> 9
+console.log(addThenMultiply(...inputs));
