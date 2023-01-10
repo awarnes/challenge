038fc02ef7faa8e017ef161600fc69fe4c031a35
awarnes <alex.warnes@moovel.com>
2023-01-09 21:00:49 -0800

    First pass on dedup-and-sort.js

--- dedup-and-sort.js
+++ dedup-and-sort.js
@@ -5,3 +5,10 @@
  * - remove the duplicate values
  * - and sort in ascending order.
  */
+
+const dedupAndSort = array => [...new Set(array)].sort((a, b) => a - b);
+
+const question = [5, 6, 7, 24, 24, 109, 5, 13, 0, 3, 1];
+const answer = [0, 1, 3, 5, 6, 7, 13, 24, 109];
+
+assert(dedupAndSort(question).every((val, ind) => val === answer[ind]));
