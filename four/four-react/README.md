366dd97b4caeb1ecdc3a22d17afb202fdc159c72
awarnes <alex.warnes@moovel.com>
2023-01-23 13:14:47 -0800

    Made a thing :/

--- four/four-react/README.md
+++ four/four-react/README.md
@@ -1,1 +1,24 @@
 # React.js Practice
+
+
+## Requirements:
+ - Fetch hotels:
+  - POST https://experimentation-staging.snaptravel.com/interview/hotels
+  JSON request structure:
+  {
+    city: city_string_input,
+    checkin: checkin_string_input,
+    checkout: checkout_string_input,
+    provider: 'snaptravel'
+  }
+
+ - Display hotels
+   - name
+   - number of stars
+   - city (address)
+   - price
+
+- add `book` button
+  - when clicked books hotel
+  - display alert
+    - "You've booked the hotel"
--- four/four-react/src/App.js
+++ four/four-react/src/App.js
@@ -1,21 +1,48 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
+const BASE_HOTEL_ROUTE = 'https://experimentation-staging.snaptravel.com/interview/hotels';
+
+const getHotels = async () => {
+  return await fetch(BASE_HOTEL_ROUTE, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      city: 'city_string_input',
+      checkin: 'checkin_string_input',
+      checkout: 'checkout_string_input',
+      provider: 'snaptravel'
+    })
+  })
+  .then(resp => resp.json())
+  .then(json => json.hotels)
+  .catch(error => (console.error('error: ', error)));
+};
+
 function App() {
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    getHotels()
+      .then(hotels => setHotels(hotels));
+  }, []);
+  console.log(setHotels)
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {hotels.map(hotel => (
+          <li key={hotel.id}>
+            <p>Name: {hotel.hotel_name}</p>
+            <p>Address: {hotel.address}</p>
+            <p>Price: {hotel.price}</p>
+            <p>Stars: {hotel.num_stars}</p>
+            <button type='button' onClick={() => alert(`You've successfully booked your stay ${hotel.hotel_name}`)}>Book</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
